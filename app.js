cf195fea4daa508d6ef9abbb5438dfcb7d920da4
Ubuntu <ubuntu@ip-172-31-94-208.ec2.internal>
2022-10-06 22:34:31 +0000

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Developers!"));
+app.get("/", (req, res) => res.send("Hello Developers, lets look CICD in action!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
